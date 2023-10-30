8d5c72d913c324aa71b30c08684567b8e1d1ff5a
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 21:10:39 +0300

    Add get_transports_list transport service

--- services/transport.py
+++ services/transport.py
@@ -43,3 +43,22 @@ async def delete_transport_by_id(id: int, session: AsyncSession):
     stmt = delete(Transport).where(Transport.id == id)
     await session.execute(stmt)
     await session.commit()
+
+
+async def get_transports_list(
+    start: int,
+    count: int,
+    transport_type: str,
+    session: AsyncSession,
+):
+    if transport_type == "All":
+        query = select(Transport).offset(start).limit(count)
+    else:
+        query = (
+            select(Transport)
+            .where(Transport.transport_type == transport_type)
+            .offset(start)
+            .limit(count)
+        )
+    result = await session.execute(query)
+    return [account[0] for account in result.all()]
