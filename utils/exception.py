b7afdf1a32f64a849cdc186b1b165e47dec3790d
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 20:51:57 +0300

    Add access_denied_exception

--- utils/exception.py
+++ utils/exception.py
@@ -4,3 +4,8 @@ user_not_found_exception = HTTPException(
     status_code=404,
     detail="User not found",
 )
+
+access_denied_exception = HTTPException(
+    status_code=403,
+    detail="Access denied",
+)
