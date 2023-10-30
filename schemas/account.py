3f833b00e8d9e95468863b86e34a65d6ee0a7d07
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 18:22:30 +0300

    Add AccountBase schema

--- schemas/account.py
+++ schemas/account.py
@@ -5,3 +5,13 @@ class AccountBalance(BaseModel):
     id: int
     username: str
     balance: float
+
+
+class AccountBase(AccountBalance):
+    is_admin: bool = Field(
+        alias="isAdmin",
+    )
+
+    class Config:
+        from_attributes = True
+        populate_by_name = True
