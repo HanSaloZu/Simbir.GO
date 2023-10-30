02fee378f30180ea1010659bcd3e0e0b5d65e485
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 18:34:28 +0300

    Add get_account_by_username account service

--- services/account.py
+++ services/account.py
@@ -1,4 +1,8 @@
 from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models.account import Account
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +13,10 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return password_context.verify(password, hashed_password)
+
+
+async def get_account_by_username(username: str, session: AsyncSession):
+    query = select(Account).where(Account.username == username)
+    result = await session.execute(query)
+    account = result.one_or_none()
+    return None if account is None else account[0]
