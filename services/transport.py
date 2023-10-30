ce6f5a022548941dd67c8786763134f957d83f63
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 20:53:49 +0300

    Add update_transport transport service

--- services/transport.py
+++ services/transport.py
@@ -11,6 +11,24 @@ async def get_transport_by_id(id: int, session: AsyncSession):
     return None if transport is None else transport[0]
 
 
+async def update_transport(
+    data,
+    transport: Transport,
+    session: AsyncSession,
+):
+    transport.can_be_rented = data["can_be_rented"]
+    transport.model = data["model"]
+    transport.color = data["color"]
+    transport.identifier = data["identifier"]
+    transport.description = data["description"]
+    transport.latitude = data["latitude"]
+    transport.longitude = data["longitude"]
+    transport.minute_price = data["minute_price"]
+    transport.day_price = data["day_price"]
+    await session.commit()
+    return transport
+
+
 async def delete_transport_by_id(id: int, session: AsyncSession):
     stmt = delete(Transport).where(Transport.id == id)
     await session.execute(stmt)
