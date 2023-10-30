f67f75a706744b36583e8231cce5dc595d78f859
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 18:46:36 +0300

    Add POST /api/Account/SignUp AccountController endpoint

--- routers/account.py
+++ routers/account.py
@@ -7,11 +7,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
 from models.account import Account
-from schemas.account import AccountBase
+from schemas.account import AccountBase, AccountCreateUpdate
 from schemas.token import TokenBase
-from services.account import get_account_by_username, verify_password
+from services.account import (create_account, get_account_by_username,
+                              verify_password)
 from services.token import create_token
 from utils.auth import get_current_account
+from utils.response import nonunique_username_response
 
 router = APIRouter()
 
@@ -47,3 +49,19 @@ async def login(
         status_code=400,
         content=jsonable_encoder({"detail": "Invalid username or password"}),
     )
+
+
+@router.post(
+    "/SignUp",
+    status_code=201,
+    response_model=AccountBase,
+    description="Регистрация нового аккаунта",
+)
+async def register(
+    data: AccountCreateUpdate,
+    session: AsyncSession = Depends(get_async_session),
+):
+    account = await get_account_by_username(data.username, session)
+    if account:
+        return nonunique_username_response
+    return await create_account({**data.model_dump()}, session)
