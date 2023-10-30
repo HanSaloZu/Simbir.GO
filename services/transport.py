480b59eef82da80c2d695dcd6238c147dd41761d
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 20:53:27 +0300

    Add delete_transport_by_id transport service

--- services/transport.py
+++ services/transport.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.transport import Transport
@@ -9,3 +9,9 @@ async def get_transport_by_id(id: int, session: AsyncSession):
     result = await session.execute(query)
     transport = result.one_or_none()
     return None if transport is None else transport[0]
+
+
+async def delete_transport_by_id(id: int, session: AsyncSession):
+    stmt = delete(Transport).where(Transport.id == id)
+    await session.execute(stmt)
+    await session.commit()
