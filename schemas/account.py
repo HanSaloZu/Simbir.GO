3c2b61da998ad158e3cfbbe34ed32bdf2c577d60
HanSaloZu <conswintcomin@gmail.com>
2023-10-30 18:48:49 +0300

    Add AccountAdminCreateUpdate schema

--- schemas/account.py
+++ schemas/account.py
@@ -20,3 +20,12 @@ class AccountBase(AccountBalance):
 class AccountCreateUpdate(BaseModel):
     username: str = Field(min_length=1, max_length=250)
     password: str = Field(min_length=1, max_length=250)
+
+
+class AccountAdminCreateUpdate(AccountCreateUpdate):
+    is_admin: bool = Field(alias="isAdmin")
+    balance: float
+
+    class Config:
+        from_attributes = True
+        populate_by_name = True
