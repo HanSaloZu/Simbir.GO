d586c703ad431fced4710759fe310d637038b65b
HanSaloZu <conswintcomin@gmail.com>
2023-11-02 21:18:23 +0300

    Add GET /api/Rent/{rentId} RentController endpoint

--- routers/rent.py
+++ routers/rent.py
@@ -2,8 +2,14 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
+from models.account import Account
+from schemas.rent import RentBase
 from schemas.transport import ExtendedTransportType, TransportBase
-from services.transport import get_transports_available_for_rent
+from services.rent import get_rent_by_id
+from services.transport import (get_transport_by_id,
+                                get_transports_available_for_rent)
+from utils.auth import get_current_account
+from utils.exception import access_denied_exception, rent_not_found_exception
 
 router = APIRouter()
 
@@ -27,3 +33,23 @@ async def get_transports_for_rent(
         type,
         session,
     )
+
+
+@router.get(
+    "/{id}",
+    response_model=RentBase,
+    description="Получение информации об аренде по id",
+)
+async def get_rent_info(
+    id: int,
+    account: Account = Depends(get_current_account),
+    session: AsyncSession = Depends(get_async_session),
+):
+    rent = await get_rent_by_id(id, session)
+    if rent:
+        transport = await get_transport_by_id(rent.transport_id, session)
+        if rent.renter_id == account.id or transport.owner_id == account.id:
+            return rent
+        else:
+            raise access_denied_exception
+    raise rent_not_found_exception
