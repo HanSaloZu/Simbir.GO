555518e54cf16a10a99607e7ce0858c1011a8ce1
HanSaloZu <conswintcomin@gmail.com>
2023-11-02 21:05:46 +0300

    Add is_transport_in_rent rent service

--- services/rent.py
+++ services/rent.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.rent import Rent
@@ -11,3 +12,12 @@ async def create_rent(
     session.add(rent)
     await session.commit()
     return rent
+
+
+async def is_transport_in_rent(
+    transport_id: int,
+    session: AsyncSession,
+) -> bool:
+    query = select(Rent).where(Rent.time_end == None, Rent.transport_id == transport_id)
+    result = await session.execute(query)
+    return result.one_or_none() is not None
